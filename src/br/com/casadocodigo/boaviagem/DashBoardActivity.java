83ccd37ce0dacdc48d545d5c35d7e535dcf480dc
Lucas <lcastro.oliveira@gmail.com>
2013-07-21 17:05:29 -0300

    Continuação da contrução das telas

--- src/br/com/casadocodigo/boaviagem/DashBoardActivity.java
+++ src/br/com/casadocodigo/boaviagem/DashBoardActivity.java
@@ -1,7 +1,27 @@
 package br.com.casadocodigo.boaviagem;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class DashBoardActivity extends Activity{
-
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.dashboard);
+	}
+	
+	public void selecionaOpcao(View view) {
+		/**
+		 * com base na opção que foi clicada iremos tomar uma ação
+		 */		
+		TextView textView = (TextView)view;		
+		String opcao = "Opcao: "+textView.getText().toString();
+		Toast.makeText(this, opcao, Toast.LENGTH_LONG).show();
+	}
 }
